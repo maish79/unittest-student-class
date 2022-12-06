28a9c8ed9dd9d24f03e2f791e680fb2b45565bf0
maish79 <maishsupa@yahoo.com>
2022-12-06 12:11:54 +0000

    Add mocking functions

--- student.py
+++ student.py
@@ -1,9 +1,12 @@
 from datetime import date, timedelta
+import requests
+
 
 class Student:
     """ A Student class as a basis for method testing """
+
     def __init__(self, first_name, last_name):
-        self._first_name  = first_name
+        self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
@@ -19,3 +22,15 @@ class Student:
 
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
